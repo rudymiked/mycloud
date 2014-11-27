d08b573dc77e7734f5956bc3a7248f110826fd58
Rudy <mru222@g.uky.edu>
2014-11-26 23:50:53 -0500

    just definitions of funcs

---
+++ LibCalls.c
@@ -0,0 +1,27 @@
+
+
+int mycloud_putfile(char *MachineName, int TCPport, int SecretKey, char *FileName, char *data, int datalen) {
+
+}
+
+
+int mycloud_getfile(char *MachineName, int TCPport, int SecretKey, char *Filename, char *data, int datalen) {
+
+
+
+
+}
+
+int mycloud_delfile(char *MachineName, int TCPport, int SecretKey, char *Filename) { 
+
+
+
+}
+
+
+int mycloud_listfiles(char *MachineName, int TCPport, int SecretKey, char *listbuf, int listbuflen) {
+
+
+
+
+}
