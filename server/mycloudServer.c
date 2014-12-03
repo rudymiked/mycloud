02015a223fe349bb15e51e2f56e4b4bbf9e9f4d8
Russell Brooks <russellbrooks867@gmail.com>
2014-12-03 16:20:58 -0500

    updated server with secret key

--- server/mycloudServer.c
+++ server/mycloudServer.c
@@ -7,11 +7,11 @@ RUDY BROOKS
 ************************/
 
 #include <stdio.h>
-#include "../LibCalls.c"
+#include <stdlib.h>
 #include "../mycloud.h"
 #include "csapp.h"
 
-void echo(int connfd);
+int validKey(int connfd, unsigned int secretKey);
 
 int main(int argc, char **argv) 
 {
@@ -20,11 +20,14 @@ int main(int argc, char **argv)
     struct sockaddr_in clientaddr;
     struct hostent *hp;
     char *haddrp;
-    if (argc != 2) {
-	fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    unsigned int secretKey;
+
+    if (argc != 3) {
+	fprintf(stderr, "usage: %s <port> <secret key>\n", argv[0]);
 	exit(0);
     }
     port = atoi(argv[1]);
+    secretKey = atoi(argv[2]);
 
     listenfd = Open_listenfd(port);
     while (1) {
@@ -37,9 +40,32 @@ int main(int argc, char **argv)
 	haddrp = inet_ntoa(clientaddr.sin_addr);
 	printf("server connected to %s (%s)\n", hp->h_name, haddrp);
 
-	echo(connfd);
+        validKey(connfd, secretKey);
 	Close(connfd);
     }
     exit(0);
 }
+
+// returns 0 if key is valid and -1 if invalid
+int validKey(int connfd, unsigned int secretKey) {
+    size_t n;
+    char buf[SECRET_KEY_SIZE];
+    rio_t rio;
+
+    Rio_readinitb(&rio, connfd);
+    if((n = Rio_readnb(&rio, buf, SECRET_KEY_SIZE)) == 4) {
+        printf("server received %d bytes\n", (int)n);
+        printf("server's secret key is %d\n", secretKey);
+        if(atoi(buf) == secretKey) {
+            printf("server authenticated the secret key %d\n", atoi(buf));
+            return 0;
+        } else {
+            printf("invalid secret key %d\n", atoi(buf));
+            return -1;
+        }
+    }    
+    return -1;
+}
+
+
 
