0711d6cc3eaa66d92a1e8671def0ed4883c99d90
Russell Brooks <russellbrooks867@gmail.com>
2014-12-09 22:45:50 -0500

    updated mclist

--- client/mclist.c
+++ client/mclist.c
@@ -10,19 +10,26 @@ RUDY BROOKS
 #include "stdio.h"
 #include "stdlib.h"
 
-
 int main(int argc, char *argv[]){
 
-  int list_buflen;
+  int list_len;
   char *list_buf;
 
   char *machine_name = argv[1];
   unsigned int tcp_port     = atoi(argv[2]);
   unsigned int secret_key   = atoi(argv[3]);  
 
-  mycloud_listfiles(machine_name, tcp_port, secret_key, list_buf, list_buflen);
+  int status = mycloud_listfiles(machine_name, tcp_port, secret_key, &list_buf, &list_len);
 
-  printf("%s \n", list_buf);
+  if(status == 0) {
+    int i;
+    char fileName[FILE_NAME_SIZE];
+    for(i = 0; i < list_len; i += FILE_NAME_SIZE) {
+      memcpy(&fileName, list_buf + i, FILE_NAME_SIZE);
+      printf("%s\n", fileName);
+    }
+  }
 
-  return 0;
+  free(list_buf);
+  return status;
 }
